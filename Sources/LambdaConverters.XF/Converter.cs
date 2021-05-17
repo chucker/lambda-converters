2a70e69cc78facd4aebba0b939f6e4c9bad4f498
Sören Kuklau <chucker23n+github@gmail.com>
2021-05-17 17:59:55 +0200

    Early stab at porting some of this to Xamarin Forms

---
+++ Sources/LambdaConverters.XF/Converter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace LambdaConverters
+{
+    internal abstract class Converter
+    {
+        protected Converter(
+            ConverterErrorStrategy errorStrategy,
+            object? defaultInputTypeValue,
+            object? defaultOutputTypeValue,
+            Type inputType,
+            Type outputType,
+            bool isConvertFunctionAvailable,
+            bool isConvertBackFunctionAvailable)
+        {
+            ErrorStrategy = errorStrategy;
+            DefaultInputTypeValue = defaultInputTypeValue;
+            DefaultOutputTypeValue = defaultOutputTypeValue;
+            InputType = inputType;
+            OutputType = outputType;
+            IsConvertFunctionAvailable = isConvertFunctionAvailable;
+            IsConvertBackFunctionAvailable = isConvertBackFunctionAvailable;
+        }
+
+        internal ConverterErrorStrategy ErrorStrategy { get; }
+
+        internal object? DefaultInputTypeValue { get; }
+
+        internal object? DefaultOutputTypeValue { get; }
+
+        internal Type InputType { get; }
+
+        internal Type OutputType { get; }
+
+        internal bool IsConvertFunctionAvailable { get; }
+
+        internal bool IsConvertBackFunctionAvailable { get; }
+
+        internal object? GetErrorValue(object? defaultValue)
+            => ErrorStrategy switch
+            {
+                ConverterErrorStrategy.ReturnDefaultValue => defaultValue,
+                ConverterErrorStrategy.UseFallbackOrDefaultValue => null,
+                ConverterErrorStrategy.DoNothing => Binding.DoNothing,
+                _ => throw new NotSupportedException()
+            };
+    }
+}
---
+++ Sources/Tests.LambdaConverters.XF/ValueConverterTests.cs
@@ -0,0 +1,48 @@
+using System.Globalization;
+using LambdaConverters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.LambdaConverters.XF
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void WithConvertFunction()
+        {
+            // with a wrong target type (use default error strategy)
+            Assert.IsNull(ValueConverter.Create<int, string?>(e => null).Convert(1, typeof(bool), null, null));
+
+            // without a target type
+            Assert.AreEqual("a", ValueConverter.Create<int, string>(e => "a").Convert(1, null, null, null));
+
+            // with an unexpected parameter (use default error strategy)
+            Assert.IsNull(ValueConverter.Create<int, string?>(e => null).Convert(1, typeof(string), "p", null));
+
+            // with an input value of an unexpected type (use default error strategy)
+            Assert.IsNull(ValueConverter.Create<int, string?>(e => null).Convert(true, typeof(string), null, null));
+            Assert.IsNull(ValueConverter.Create<int, string?>(e => null).Convert(null, typeof(string), null, null));
+
+            // with a valid input value
+            Assert.AreEqual("a", ValueConverter.Create<int, string>(e => "a").Convert(1, typeof(string), null, null));
+            Assert.AreEqual("1", ValueConverter.Create<int, string>(
+                e =>
+                {
+                    Assert.AreEqual(1, e.Value);
+                    Assert.IsNull(e.Culture);
+
+                    return e.Value.ToString();
+                }).Convert(1, typeof(string), null, null));
+            Assert.AreEqual(
+                "1",
+                ValueConverter.Create<int, string>(
+                    e =>
+                    {
+                        Assert.AreEqual(1, e.Value);
+                        Assert.AreEqual(new CultureInfo("en-GB"), e.Culture);
+
+                        return e.Value.ToString();
+                    }).Convert(1, typeof(string), null, new CultureInfo("en-GB")));
+        }
+    }
+}
